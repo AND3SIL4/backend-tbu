81fdfa23941de1a9ccac818c444f179c41195b1d
AND3SIL4 <afsilva6236@misena.edu.co>
2024-12-17 16:05:44 -0500

    test(app) new endpoint added

--- api/app.js
+++ api/app.js
@@ -20,7 +20,7 @@ app.get("/", (req, res) => {
   });
 });
 
-app.get("/hello-world", (req, res) => {
+app.get("/hello", (req, res) => {
   res.send({
     message: "Hello world!",
   });
--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 import app from "./api/app.js";
-import { config } from "dotenv";
-
-// Load configuration
-config();
 
 // Set the main port
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 app.listen(PORT, () => {
   console.log(`Proxy server is running on port ${PORT}`);
